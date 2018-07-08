7cd6e164db64121eb1cf995aee1fb39441c7bbc1
Nuno Faustino <faustnun@rg.im>
2018-07-08 16:03:31 +0100

    Remove irrelevant variables

--- Script.R
+++ Script.R
@@ -24,9 +24,43 @@ ipak <- function(pkg){
 # Warning this will install the packages you don't have
 ipak(packages)
 
-
+# Fread is crashing with test.csv
 #tr <- fread("input/train.csv", drop = "ID", header = T, showProgress = F)
 #te <- fread("input/test.csv", drop = "ID", header = T, showProgress = F)
 train <- readr::read_csv("input/train.csv")
 test  <- readr::read_csv("input/test.csv")
-                                                                                                                     
+
+# Removing ID
+train$ID <- NULL
+test.id <- test$ID
+test$ID <- NULL
+
+##### Extracting TARGET
+train.y <- train$target
+train$target <- NULL
+
+
+
+##### Removing constant features
+cat("\n## Removing the constants features.\n")
+for (f in names(train)) {
+  if (length(unique(train[[f]])) == 1) {
+    cat(f, "is constant in train. We delete it.\n")
+    train[[f]] <- NULL
+    test[[f]] <- NULL
+  }
+}
+
+
+## Removing incomplete cases (6)
+cat("\n## Removing the incomplete oservations\n")
+train <- train[complete.cases(train),]
+
+
+#Removing correlated columns 0.9
+corMatrix <- cor(train)
+toRemove <- findCorrelation(corMatrix, cutoff = .90, verbose = T)
+toRemove <- sort(toRemove)
+
+train <- train[,-c(toRemove)]
+test <- test[,-c(toRemove)]
